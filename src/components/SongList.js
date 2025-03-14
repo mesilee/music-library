d9ae0f63db71b30885e88bd3a72593f42182d758
meseret <mmeseretayalew@gmail.com>
2025-03-14 15:05:17 +0300

    Style SongList component using Emotion

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,6 +1,27 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchSongsRequest } from '../redux/songs/songsSlice';
+import styled from '@emotion/styled';
+
+const SongListContainer = styled.div`
+  padding: 20px;
+  font-family: sans-serif;
+`;
+
+const SongListTitle = styled.h2`
+  color: #333;
+  margin-bottom: 10px;
+`;
+
+const SongListUl = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+const SongListItem = styled.li`
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+`;
 
 function SongList() {
   const dispatch = useDispatch();
@@ -14,14 +35,14 @@ function SongList() {
   if (error) return <p>Error: {error}</p>;
 
   return (
-    <div>
-      <h2>Song List</h2>
-      <ul>
+    <SongListContainer>
+      <SongListTitle>Song List</SongListTitle>
+      <SongListUl>
         {songs.map((song) => (
-          <li key={song.id}>{song.title}</li>
+          <SongListItem key={song.id}>{song.title}</SongListItem>
         ))}
-      </ul>
-    </div>
+      </SongListUl>
+    </SongListContainer>
   );
 }
 
