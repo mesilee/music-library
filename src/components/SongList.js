0c53d068d66a96a9db2fad745322dfd049e7a58c
meseret <mmeseretayalew@gmail.com>
2025-03-14 14:54:23 +0300

    Connect SongList component to Redux store

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchSongsRequest } from '../redux/songs/songsSlice';
 
 function SongList() {
+  const dispatch = useDispatch();
+  const { songs, loading, error } = useSelector((state) => state.songs);
+
+  useEffect(() => {
+    dispatch(fetchSongsRequest());
+  }, [dispatch]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
   return (
     <div>
       <h2>Song List</h2>
       <ul>
-        <li>Song 1</li>
-        <li>Song 2</li>
-        <li>Song 3</li>
+        {songs.map((song) => (
+          <li key={song.id}>{song.title}</li>
+        ))}
       </ul>
     </div>
   );
